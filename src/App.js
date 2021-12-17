524f3c3ac53e02929c5e5e749b5002e0ed3d5566
RameshNeupane <neupaneramesh555@gmail.com>
2021-12-17 22:09:18 +0545

    update /src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
