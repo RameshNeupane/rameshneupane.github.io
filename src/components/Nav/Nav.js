9246203c8ef3425a9b6cbde2a068090c24f50e20
RameshNeupane <neupaneramesh555@gmail.com>
2022-01-28 11:49:03 +0545

    update Link component to NavLink component for active class

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,35 +1,19 @@
 import React from "react";
 import "./Nav.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-  const clickHandle = () => {
-    const navTabs = document.querySelectorAll(".tab");
-    navTabs.forEach((navTab) => {
-      navTab.addEventListener("click", () => {
-        removeActiveTabClass();
-        navTab.classList.add("tab-active");
-      });
-    });
-
-    const removeActiveTabClass = () => {
-      navTabs.forEach((navTab) => {
-        navTab.classList.remove("tab-active");
-      });
-    };
-  };
-
   return (
     <nav className="Nav">
-      <Link to="/about" className="tab" onMouseOver={clickHandle}>
+      <NavLink to="/about" className="tab">
         About
-      </Link>
-      <Link to="/" className="tab" onMouseOver={clickHandle}>
+      </NavLink>
+      <NavLink to="/" className="tab">
         R N
-      </Link>
-      <Link to="/contact" className="tab" onMouseOver={clickHandle}>
+      </NavLink>
+      <NavLink to="/contact" className="tab">
         Contact
-      </Link>
+      </NavLink>
     </nav>
   );
 };
