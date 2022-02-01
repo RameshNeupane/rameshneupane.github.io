351625d441f5f26b976ebecf224a25504c92c276
RameshNeupane <neupaneramesh555@gmail.com>
2022-02-01 19:13:25 +0545

    update about and contact component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,61 @@
 import React from "react";
 import "./About.css";
+import {
+  SiC,
+  SiCplusplus,
+  SiCss3,
+  SiHtml5,
+  SiJavascript,
+  SiPython,
+  SiGit,
+  SiNumpy,
+  SiPandas,
+  SiTensorflow,
+  SiKeras,
+  SiReact,
+} from "react-icons/si";
 
 const About = () => {
   return (
-    <div className="about">
-      <h1>About Page</h1>
+    <div className="about-container">
+      <div className="about">
+        <h1>About</h1>
+        <hr />
+        <p>
+          Hello there, I am a front-end developer from Kathmandu, Nepal. I
+          mainly use React.
+          <br />
+          <br />I am also an Artificial Intelligence (AI) enthusiast. I am
+          learning Machine Learning algorithms and I create solutions for real
+          world scenarios.
+        </p>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <hr />
+        <div className="skills-container">
+          <div className="basic-lang">
+            <SiC title="C Programming" />
+            <SiCplusplus title="C++" />
+            <SiPython title="Python" />
+          </div>
+          <hr />
+          <div className="frontend">
+            <SiHtml5 title="HTML" />
+            <SiCss3 title="CSS" />
+            <SiJavascript title="JavaScript" />
+            <SiReact title="React" />
+            <SiGit title="Git" />
+          </div>
+          <hr />
+          <div className="deep-learning">
+            <SiNumpy title="Numpy" />
+            <SiPandas title="Pandas" />
+            <SiTensorflow title="Tensorflow" />
+            <SiKeras title="Keras" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,48 @@
 import React from "react";
 import "./Contact.css";
+import {
+  FaEnvelope,
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaPhoneAlt,
+} from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <h1>Contact Page</h1>
+      <div className="github">
+        <a href="https://github.com/RameshNeupane" target="_black">
+          <FaGithub title="Github" />
+        </a>
+      </div>
+      <div className="social-media">
+        <a
+          rel="noreferrer"
+          href="https://www.facebook.com/rawmesh.neupane.9/"
+          target="_blank"
+        >
+          <FaFacebook title="Facebook" />
+        </a>
+        <a
+          rel="noreferrer"
+          href="https://www.instagram.com/__maailo/"
+          target="_blank"
+        >
+          <FaInstagram title="Instagram" />
+        </a>
+      </div>
+      <div className="email">
+        <FaEnvelope title="Email" />
+        <hr />
+        <p>neupaneramesh555@gmail.com</p>
+      </div>
+      <div className="mobile">
+        <FaPhoneAlt title="Mobile" />
+        <hr />
+        <p title="ntc">9866255970</p>
+        <p title="ncell">9808223532</p>
+      </div>
     </div>
   );
 };
