c522c7f4c44e4decf7b8c6066639fa032156ead1
RameshNeupane <neupaneramesh555@gmail.com>
2022-02-01 19:20:35 +0545

    nav component update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,15 +5,19 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="Nav">
-      <NavLink to="/about" className="tab">
-        About
-      </NavLink>
-      <NavLink to="/" className="tab">
-        R N
-      </NavLink>
-      <NavLink to="/contact" className="tab">
-        Contact
-      </NavLink>
+      <div>
+        <NavLink to="/" className="tab">
+          R N
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/about" className="tab">
+          About
+        </NavLink>
+        <NavLink to="/contact" className="tab">
+          Contact
+        </NavLink>
+      </div>
     </nav>
   );
 };
