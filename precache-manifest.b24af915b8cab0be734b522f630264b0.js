2c6103f7c253af06792624bfd450971754088331
RameshNeupane <neupaneramesh555@gmail.com>
2022-06-26 10:31:30 +0545

    Updates

---
+++ precache-manifest.b24af915b8cab0be734b522f630264b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d62463ffc91965925b07",
+    "url": "/static/css/main.28538a7c.chunk.css"
+  },
+  {
+    "revision": "d62463ffc91965925b07",
+    "url": "/static/js/main.db5f7255.chunk.js"
+  },
+  {
+    "revision": "35fce4b6b1814273d46b",
+    "url": "/static/js/runtime~main.95549f2e.js"
+  },
+  {
+    "revision": "3773cb122afeb36959ec",
+    "url": "/static/js/2.25444d5b.chunk.js"
+  },
+  {
+    "revision": "5f721e493ae4611d3ee7",
+    "url": "/static/js/3.b19e92eb.chunk.js"
+  },
+  {
+    "revision": "447c48641261fd54b83e55eb2cc54d4a",
+    "url": "/static/media/background.447c4864.jpg"
+  },
+  {
+    "revision": "85a8586266d677950d8676a23ba32c05",
+    "url": "/index.html"
+  }
+];
